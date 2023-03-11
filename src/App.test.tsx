b1115c1156e027c1d0c439c68d30a0e16aa0e781
Marcin Jędrzejewski <jedrzejewski.marcin@gmail.com>
2023-03-11 18:29:11 +0100

    Setup linters

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { render } from "@testing-library/react";
-import { App } from "./App";
+import { render } from '@testing-library/react'
+import { App } from './App'
 
-test("renders nothing", () => {
-  const { container } = render(<App />);
-  expect(container).toBeEmptyDOMElement();
-});
+test('renders nothing', () => {
+  const { container } = render(<App />)
+  expect(container).toBeEmptyDOMElement()
+})
