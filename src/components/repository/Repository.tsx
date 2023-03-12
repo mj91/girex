0f67d6eafed00deb515349e9a8f0e5bb5b8892cc
Marcin Jędrzejewski <jedrzejewski.marcin@gmail.com>
2023-03-12 05:22:22 +0100

    Remove empty space from repository when description is empty

--- src/components/repository/Repository.tsx
+++ src/components/repository/Repository.tsx
@@ -3,20 +3,21 @@ import { Card, CardContent, CardHeader, Grid, Box } from '@mui/material'
 import { Repo } from '../../services/github-api'
 
 export const Repository = ({ repo }: { repo: Repo }) => {
+  const { name, stargazers_count, description } = repo
   return (
     <Card>
       <CardHeader
         title={
           <Grid container justifyContent="space-between">
-            <Grid item>{repo.name}</Grid>
+            <Grid item>{name}</Grid>
             <Grid item xs="auto" container>
-              <Box mr={1}>{repo.stargazers_count}</Box>
+              <Box mr={1}>{stargazers_count}</Box>
               <Star fontSize="medium" />
             </Grid>
           </Grid>
         }
       />
-      <CardContent>{repo.description}</CardContent>
+      {description && <CardContent>{description}</CardContent>}
     </Card>
   )
 }
